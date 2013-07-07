7f46d934383c14ff532507e7ba77c56edc0d10ab
Damian Spain <dmspain@yahoo.com>
2013-07-07 21:35:07 +0000

    Using index.html rather than hardcoded string.

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync('index.html')))
 });
 
 var port = process.env.PORT || 5000;
